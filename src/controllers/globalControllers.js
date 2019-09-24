aa39ae0006ed8119e48052ab3bafb5ec8e2883d4
choiwh <dashiki78@gmail.com>
2019-09-24 12:59:14 +0900

    user athenticate

--- src/controllers/globalControllers.js
+++ src/controllers/globalControllers.js
@@ -1,6 +1,57 @@
+import passport from "passport";
+import User from "../models/User";
+import routes from "../routes";
+
 export const home = (req, res) => {
     res.render("home", { pageTitle: "HOME"});
 };
+
+export const getJoin = (req, res) => {
+    res.render("join");
+};
+
+export const postJoin = async (req, res, next) => {
+    const {
+        body: { name, email, password, password2 }
+    } = req;
+    if (password !== password2) {
+        req.flash("error", "비밀번호가 일치하지 않습니다");
+        res.status(400);
+        res.render("join");
+    } else {
+        try {
+            const user = await User({
+                nickName: name,
+                email
+            });
+            await User.register(user, password);
+            next();            
+        } catch (error) {
+            console.log(error);
+            if (error.name === 'UserExistsError') {
+                req.flash("error", "이미 가입된 유저입니다");
+            }
+            res.redirect(routes.home);
+        }
+    }
+};
+
+export const getLogin = (req, res) => {
+    res.render("login");
+};
+
+export const postLogin = passport.authenticate("local", {
+    failureRedirect: routes.login,
+  successRedirect: routes.home,
+  successFlash: "환영합니다",
+  failureFlash: "로그인에 실패하였습니다. 이메일 또는 비밀번호를 확인하세요"
+});
+
+export const logout = (req, res) => {
+    req.flash("info", "로그아웃 되었습니다");
+    req.logout();
+    res.redirect(routes.home);
+  };
 
 export const changePassword = (req, res) => {
     res.render("changePassword");
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
 import routes from "../routes";
-import { home, changePassword } from "../controllers/globalControllers";
+import { home, changePassword, getJoin, postJoin, getLogin, postLogin, logout } from "../controllers/globalControllers";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
+globalRouter.get(routes.logout, onlyPrivate, logout);
 globalRouter.get(routes.changePassword, changePassword);
 
 
